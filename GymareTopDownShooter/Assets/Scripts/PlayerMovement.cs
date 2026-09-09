b14b87d6c2c29f3ab46f2ea57f543f067c0c26de
Freakbert <edwin.skoglund@elev.ga.lbs.se>
2026-09-09 11:18:06 +0200

    ny skit

--- GymareTopDownShooter/Assets/Scripts/PlayerMovement.cs
+++ GymareTopDownShooter/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] KeyCode moveUp;
-    [SerializeField] KeyCode moveDown;
-    [SerializeField] KeyCode moveRight;
-    [SerializeField] KeyCode moveLeft;
+    float moveVert;
     Vector3 moveDir => new Vector3(0,0,0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
