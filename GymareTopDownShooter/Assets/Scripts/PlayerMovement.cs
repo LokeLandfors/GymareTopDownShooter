d0ca48a2493b193acb296ef8f212f403b8f84fa8
Freakbert <edwin.skoglund@elev.ga.lbs.se>
2026-09-10 13:34:24 +0200

    action system wip

--- GymareTopDownShooter/Assets/Scripts/PlayerMovement.cs
+++ GymareTopDownShooter/Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float moveVert;
-    Vector3 moveDir => new Vector3(0,0,0);
+    [SerializeField] float moveSpeed;
+
+    Rigidbody2D rb;
+    Vector2 MoveDir => InputSystem.actions.FindAction("Move").ReadValue<Vector2>();
+    Vector2 LookDir => InputSystem.actions.FindAction("Aim").ReadValue<Vector2>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.MovePosition((Vector2)transform.position + MoveDir * moveSpeed * Time.fixedDeltaTime);
+        print(LookDir);
     }
 }
